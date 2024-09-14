957ba8b1242b2bff0cab3a551d534df21903cb98
Stars <bing@starsxu.com>
2024-09-14 15:04:56 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.starsxu.androidanyview"
         minSdk = 24
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.11"
+        versionCode = 1
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
